953cf12c2948527fd8ff535d7420294f19e81cdb
Pete Medina <petemedinacreative@gmail.com>
2023-09-28 14:06:00 -0700

    refactor(heading): update prop defaults
    Update prop defaults to be less redundant.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
 import { Button } from './components/atoms/Button';
+import { Heading } from './components/atoms/Heading';
 
 function App() {
   return (
     <>
-      <h1 className="text-brand-primary mb-8 text-5xl font-bold">Denali</h1>
+      <Heading level={1}>Denali</Heading>
       <Button
         label="Well"
         onPress={() => alert("What's the latest?")}
