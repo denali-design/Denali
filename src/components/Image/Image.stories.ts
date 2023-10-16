3ed328a01db95ebb90939f08148db33cedef4962
Trevor Greenleaf <trevor.greenleaf@yahooinc.com>
2023-10-16 08:44:07 -0700

    feat(image component)
    - adds image component

--- src/components/Image/Image.stories.ts
+++ src/components/Image/Image.stories.ts
@@ -1,18 +1,59 @@
+/**
+ * Storybook stories for the Image component.
+ *
+ * @file This file defines the Storybook stories for the Image component.
+ * @since 1.0.0
+ */
+
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Image } from './Image';
+import Image from './Image';
 
 const meta: Meta<typeof Image> = {
-  component: Image,
   title: 'Components/Data Display/Image',
-  tags: ['autodocs']
-};
+  component: Image,
+  tags: ['autodocs'],
+  argTypes: {
+    src: { control: 'text' },
+    alt: { control: 'text' },
+    role: { control: 'text' },
+    width: { control: 'number' },
+    height: { control: 'number' },
+    onClick: { action: 'clicked' },
+    onError: { action: 'error' },
+  },
+} as Meta;
+
 export default meta;
 
 type Story = StoryObj<typeof Image>;
-
 export const Default: Story = {
   args: {
-    children: 'Image'
-  }
+    src: 'https://placehold.co/400x300',
+    alt: 'Example image',
+  },
+};
+
+export const AltTextMissing: Story = {
+  args: {
+    src: 'https://placehold.co/400x300',
+    alt: '',
+  },
+};
+
+export const WithCustomRole: Story = {
+  args: {
+    src: 'https://placehold.co/400x300',
+    alt: 'Example image',
+    role: 'presentation',
+  },
+};
+
+export const WithDimensions: Story = {
+  args: {
+    src: 'https://placehold.co/400x300',
+    alt: 'Example image',
+    width: 400,
+    height: 300,
+  },
 };
