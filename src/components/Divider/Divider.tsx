3ce5ef205106576d8fc7a30c94535add7827f25a
Pete Medina <petemedinacreative@gmail.com>
2023-11-13 10:27:25 -0800

    feature(divider): add divider component
    - Add divider component
    - Add divider prop color
    - Add divider prop icon
    - Add divider prop label

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,12 +1,136 @@
-/**
- * Defines the Divider component.
- *
- * @file This file defines the Divider component, meant to be a reusable and accessible visual separator.
- * @since 1.0.0
- */
+import { useRef } from 'react';
+import Icon from '../Icon/Icon';
+import { tv } from 'tailwind-variants';
+import '../../App.css';
 
-const Divider: React.FC = () => {
-  return 'Divider Coming Soon';
+export type DividerColor =
+  | 'default'
+  | 'alt'
+  | 'success'
+  | 'success-vivid'
+  | 'warning'
+  | 'warning-vivid'
+  | 'info'
+  | 'info-vivid'
+  | 'danger'
+  | 'danger-vivid'
+  | 'primary'
+  | 'secondary';
+
+export type LabelColor =
+  | 'default'
+  | 'primary'
+  | 'success'
+  | 'warning'
+  | 'danger'
+  | 'info'
+  | 'on-vivid';
+
+export type Justify = 'start' | 'center' | 'end';
+
+export interface DividerProps {
+  strokeColor?: DividerColor;
+  icon?: string;
+  label?: string;
+  labelColor?: LabelColor;
+  justify?: Justify;
+}
+
+const dividerBaseClass = 'relative flex items-center w-full py-4';
+
+const colorVariants = tv({
+  variants: {
+    color: {
+      default: 'type-type',
+      primary: 'type-primary',
+      success: 'type-success',
+      warning: 'type-warning',
+      danger: 'type-danger',
+      info: 'type-info',
+      'on-vivid': 'type-on-vivid'
+    }
+  },
+  defaultVariants: {
+    color: 'default'
+  }
+});
+
+const Divider = ({
+  strokeColor = 'default',
+  icon,
+  label,
+  justify = 'center',
+  labelColor = 'default'
+}: DividerProps) => {
+  const ref = useRef(null);
+
+  // The order classes for the icon and label are set depending on the justify prop
+  let iconAndLabelOrderClass;
+  let firstHrOrderClass;
+  let secondHrOrderClass;
+
+  switch (justify) {
+    case 'start':
+      iconAndLabelOrderClass = 'order-1';
+      firstHrOrderClass = 'order-2';
+      secondHrOrderClass = 'order-3';
+      break;
+    case 'center':
+      iconAndLabelOrderClass = 'order-2';
+      firstHrOrderClass = 'order-1';
+      secondHrOrderClass = 'order-3';
+      break;
+    case 'end':
+      iconAndLabelOrderClass = 'order-3';
+      firstHrOrderClass = 'order-1';
+      secondHrOrderClass = 'order-2';
+      break;
+    default:
+      // Default case is not necessary because 'justify' prop has a default value
+      break;
+  }
+
+  const renderIcon = (iconName: string, color: LabelColor) => (
+    <Icon name={iconName} size="sm" color={color} />
+  );
+
+  return (
+    <div className={`${dividerBaseClass}`} ref={ref}>
+      <hr
+        className={`border-t-2 ${
+          strokeColor ? `stroke-${strokeColor}` : 'stroke'
+        } w-full ${firstHrOrderClass}`}
+        aria-label={label ? label : 'Divider'}
+      />
+      {icon && (
+        <div
+          className={`flex-shrink-0 px-2 ${colorVariants({
+            color: labelColor
+          })} ${iconAndLabelOrderClass}`}
+        >
+          {renderIcon(icon, labelColor)}
+        </div>
+      )}
+      {label && (
+        <p
+          className={`flex-shrink-0 px-2 ${colorVariants({
+            color: labelColor
+          })} ${iconAndLabelOrderClass}`}
+        >
+          {label}
+        </p>
+      )}
+      {(label || icon) && (
+        <hr
+          className={`border-t-2 ${
+            strokeColor ? `stroke-${strokeColor}` : 'stroke'
+          } w-full ${secondHrOrderClass}`}
+          aria-hidden="true"
+        />
+      )}
+    </div>
+  );
 };
+
 export default Divider;
 export { Divider };
