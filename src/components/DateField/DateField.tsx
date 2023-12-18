b4c558c41317e39fa9027841aae548649fac7b56
Pete Medina <petemedinacreative@gmail.com>
2023-12-18 12:27:05 -0800

    feat(date field): add date field component
    - Add datefield component
    - Update shared CSS files

--- src/components/DateField/DateField.tsx
+++ src/components/DateField/DateField.tsx
@@ -4,9 +4,48 @@
  * @file This file defines the DateField component, intended for date input.
  * @since 1.0.0
  */
+import {
+  DateField as AriaDateField,
+  Label,
+  DateInput,
+  DateSegment
+} from 'react-aria-components';
+import type {
+  DateFieldProps as AriaDateFieldProps,
+  DateValue,
+  ValidationResult
+} from 'react-aria-components';
+import { FieldError, Text } from 'react-aria-components';
+import '../../assets/css/App.css';
+import '../../assets/css/common-styles/forms/inputs.css';
+import './DateField.css';
 
-const DateField: React.FC = () => {
-  return 'DateField Coming Soon';
-};
+interface DateFieldProps<T extends DateValue> extends AriaDateFieldProps<T> {
+  label?: string;
+  description?: string;
+  errorMessage?: string | ((validation: ValidationResult) => string);
+}
+
+function DateField<T extends DateValue>({
+  label,
+  description,
+  errorMessage,
+  ...props
+}: DateFieldProps<T>) {
+  return (
+    <AriaDateField {...props}>
+      <div className="form-group">
+        <Label className="date-field__label">{label}</Label>
+        <DateInput className="date-input">
+          {(segment) => (
+            <DateSegment className="date-field__segment" segment={segment} />
+          )}
+        </DateInput>
+        {description && <Text slot="description">{description}</Text>}
+      </div>
+      <FieldError>{errorMessage}</FieldError>
+    </AriaDateField>
+  );
+}
+
 export default DateField;
-export { DateField };
