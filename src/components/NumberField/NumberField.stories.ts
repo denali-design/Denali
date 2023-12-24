eca3b91f0c515deb9adc0a0ecfc20fac091d4605
Pete Medina <petemedinacreative@gmail.com>
2023-12-23 23:47:16 -0800

    feat(NumberField): add NumberFIeld
    - Add NumberField component
    - Update Tailwind NPM package to 3.4
    - Refactor shared input styles (needs further refactor to reduce redundancy)
    - Remove custom hook from TextField (CSS solution found)

--- src/components/NumberField/NumberField.stories.ts
+++ src/components/NumberField/NumberField.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { NumberField } from './NumberField';
+import NumberField from './NumberField';
 
 const meta: Meta<typeof NumberField> = {
   component: NumberField,
@@ -13,6 +13,49 @@ type Story = StoryObj<typeof NumberField>;
 
 export const Default: Story = {
   args: {
-    children: 'NumberField'
+    label: 'Quantity',
+    description: 'How many cats do you have?'
+  }
+};
+
+export const Percents: Story = {
+  args: {
+    label: 'Sales Tax',
+    description: 'Enter the sales tax in percent',
+    defaultValue: 0.05,
+    minValue: 0,
+    formatOptions: {
+      style: 'percent'
+    }
+  }
+};
+
+export const Currency: Story = {
+  args: {
+    label: 'Total Costs',
+    defaultValue: 42,
+    step: 0.1,
+    minValue: 0,
+    formatOptions: {
+      style: 'currency',
+      currency: 'USD',
+      currencyDisplay: 'code',
+      currencySign: 'accounting'
+    }
+  }
+};
+
+export const Invalid: Story = {
+  args: {
+    label: 'Quantity',
+    isInvalid: true,
+    errorMessage: 'Enter a valid number.'
+  }
+};
+
+export const Disabled: Story = {
+  args: {
+    label: 'Quantity',
+    isDisabled: true
   }
 };
--- src/components/NumberField/NumberField.tsx
+++ src/components/NumberField/NumberField.tsx
@@ -5,8 +5,57 @@
  * @since 1.0.0
  */
 
-const NumberField: React.FC = () => {
-  return 'NumberField Coming Soon';
-};
+import {
+  NumberField as AriaNumberField,
+  Label,
+  Group,
+  Input,
+  Button
+} from 'react-aria-components';
+import type {
+  NumberFieldProps as AriaNumberFieldProps,
+  ValidationResult
+} from 'react-aria-components';
+import { FieldError, Text } from 'react-aria-components';
+import Icon from '../Icon/Icon';
+import '../../assets/css/App.css';
+import '../../assets/css/common-styles/forms/inputs.css';
+import './NumberField.css';
+
+interface NumberFieldProps extends AriaNumberFieldProps {
+  label?: string;
+  description?: string;
+  errorMessage?: string | ((validation: ValidationResult) => string);
+}
+
+function NumberField({
+  label,
+  description,
+  errorMessage,
+  ...props
+}: NumberFieldProps) {
+  return (
+    <AriaNumberField className="number-field" {...props}>
+      <div className="form-group">
+        <Label className="form-group__label-static">{label}</Label>
+        <Group className="input-group">
+          <Button className="number-field__button" slot="decrement">
+            <Icon color="default" name="icon-control-remove" size="sm" />
+          </Button>
+          <Input className="form-group__input px-2" placeholder="" />
+          <Button className="number-field__button" slot="increment">
+            <Icon color="default" name="icon-control-add" size="sm" />
+          </Button>
+        </Group>
+      </div>
+      {description && (
+        <Text className="input-description" slot="description">
+          {description}
+        </Text>
+      )}
+      <FieldError className="input-error">{errorMessage}</FieldError>
+    </AriaNumberField>
+  );
+}
+
 export default NumberField;
-export { NumberField };
