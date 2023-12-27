3f5f802a039d8609f7f3808cb8b8c81fe8445465
Pete Medina <petemedinacreative@gmail.com>
2023-12-26 18:41:09 -0800

    feat(range calendar): add range calendar
    - Add range calendar component

--- src/components/RangeCalendar/RangeCalendar.tsx
+++ src/components/RangeCalendar/RangeCalendar.tsx
@@ -5,8 +5,57 @@
  * @since 1.0.0
  */
 
-const RangeCalendar: React.FC = () => {
-  return 'RangeCalendar Coming Soon';
-};
+import {
+  Button,
+  RangeCalendar as AriaRangeCalendar,
+  CalendarCell,
+  CalendarGrid,
+  Heading
+} from 'react-aria-components';
+import type {
+  RangeCalendarProps as AriaRangeCalendarProps,
+  DateValue
+} from 'react-aria-components';
+import { Text } from 'react-aria-components';
+import Icon from '../Icon/Icon';
+import '../../assets/css/App.css';
+import '../../assets/css/common-styles/forms/dates-and-times.css';
+
+interface CalendarProps<T extends DateValue> extends AriaRangeCalendarProps<T> {
+  errorMessage?: string;
+  'aria-label'?: string;
+}
+
+function RangeCalendar<T extends DateValue>({
+  errorMessage,
+  ...props
+}: CalendarProps<T>) {
+  return (
+    <AriaRangeCalendar className="calendar" {...props}>
+      <header className="calendar__header">
+        <Button slot="previous">
+          <Icon color="default" name="icon-action-arrowhead-left" size="md" />
+        </Button>
+        <Heading className="calendar__heading" />
+        <Button slot="next">
+          <Icon color="default" name="icon-action-arrowhead-right" size="md" />
+        </Button>
+      </header>
+      <CalendarGrid className="calendar__grid">
+        {(date) => (
+          <CalendarCell
+            className="calendar__cell calendar__cell--range"
+            date={date}
+          />
+        )}
+      </CalendarGrid>
+      {errorMessage && (
+        <Text className="calendar__error-message" slot="errorMessage">
+          {errorMessage}
+        </Text>
+      )}
+    </AriaRangeCalendar>
+  );
+}
+
 export default RangeCalendar;
-export { RangeCalendar };
