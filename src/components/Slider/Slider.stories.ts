b7a479ba229abbf7946ffc539193c441b13c697e
Pete Medina <petemedinacreative@gmail.com>
2023-12-27 20:45:25 -0800

    feat(slider): add slider component
    - Add slider component
    - Add custom slider-fill functionality and classes
    - Add slider stories
    - Fix linting errors in TimeField story

--- src/components/Slider/Slider.stories.ts
+++ src/components/Slider/Slider.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Slider } from './Slider';
+import Slider from './Slider';
 
 const meta: Meta<typeof Slider> = {
   component: Slider,
@@ -13,6 +13,83 @@ type Story = StoryObj<typeof Slider>;
 
 export const Default: Story = {
   args: {
-    children: 'Slider'
+    label: 'Opacity'
+  }
+};
+
+export const MinMaxValues: Story = {
+  args: {
+    label: 'Opacity',
+    minValue: 60,
+    maxValue: 360
+  }
+};
+
+export const Currency: Story = {
+  args: {
+    label: 'Amount',
+    formatOptions: { style: 'currency', currency: 'USD' },
+    minValue: 1,
+    maxValue: 5,
+    step: 0.01,
+    defaultValue: 3
+  }
+};
+
+export const NoLabel: Story = {
+  args: {
+    'aria-label': 'Opacity'
+  }
+};
+
+export const RangeSlider: Story = {
+  args: {
+    label: 'Range',
+    thumbLabels: ['start', 'end'],
+    defaultValue: [200, 800],
+    minValue: 100,
+    maxValue: 1000
+  }
+};
+
+export const RangeSliderMultiplePoints: Story = {
+  args: {
+    label: 'Range',
+    thumbLabels: ['start', 'mid', 'end'],
+    defaultValue: [30, 50, 60],
+    minValue: 0,
+    maxValue: 100
+  }
+};
+
+export const Vertical: Story = {
+  args: {
+    label: 'Opacity',
+    minValue: 0,
+    maxValue: 100,
+    orientation: 'vertical'
+  }
+};
+
+export const VerticalRange: Story = {
+  args: {
+    label: 'Opacity',
+    thumbLabels: ['start', 'end'],
+    defaultValue: [30, 60],
+    orientation: 'vertical'
+  }
+};
+
+export const VerticalNoLabel: Story = {
+  args: {
+    'aria-label': 'Opacity',
+    orientation: 'vertical'
+  }
+};
+
+export const Disabled: Story = {
+  args: {
+    label: 'Opacity',
+    isDisabled: true
   }
 };
