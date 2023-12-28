b63f481d8bd5b3e52d5eee071d575fe99238ba5b
Pete Medina <petemedinacreative@gmail.com>
2023-12-27 16:50:50 -0800

    feat(timefield): add timefield component
    - Add TimeField component and story

--- src/components/TimeField/TimeField.stories.ts
+++ src/components/TimeField/TimeField.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
-import { TimeField } from './TimeField';
+import { Time, parseZonedDateTime } from '@internationalized/date';
+import TimeField from './TimeField';
 
 const meta: Meta<typeof TimeField> = {
   component: TimeField,
@@ -9,10 +9,76 @@ const meta: Meta<typeof TimeField> = {
 };
 export default meta;
 
+let defaultValue = new Time(8, 30);
+let minValue = new Time(11, 30);
+let maxValue = new Time(15, 30);
+let defaultValueTimezone = parseZonedDateTime(
+  '2024-04-20T16:20[America/Los_Angeles]'
+);
+
 type Story = StoryObj<typeof TimeField>;
 
 export const Default: Story = {
   args: {
-    children: 'TimeField'
+    label: 'Appointment Time'
+  }
+};
+
+export const DefaultValue: Story = {
+  args: {
+    label: 'Appointment Time',
+    defaultValue: defaultValue
+  }
+};
+
+export const DefaultValueWithTimezone: Story = {
+  args: {
+    label: 'Appointment Time',
+    defaultValue: defaultValueTimezone
+  }
+};
+
+export const MinAndMaxTimes: Story = {
+  args: {
+    label: 'Appointment Time',
+    minValue: minValue,
+    maxValue: maxValue,
+    description: 'Choose a time between 11:30 a.m. and 3:30 p.m.'
+  }
+};
+
+export const WithSeconds: Story = {
+  args: {
+    label: 'Appointment Time',
+    granularity: 'second'
+  }
+};
+
+export const HoursOnly: Story = {
+  args: {
+    label: 'Appointment Time',
+    granularity: 'hour'
+  }
+};
+
+export const TwentyFourHourClock: Story = {
+  args: {
+    label: 'Appointment Time',
+    hourCycle: 24
+  }
+};
+
+export const Invalid: Story = {
+  args: {
+    label: 'Appointment Time',
+    isInvalid: true,
+    errorMessage: 'Enter a valid time.'
+  }
+};
+
+export const Disabled: Story = {
+  args: {
+    label: 'Appointment Time',
+    isDisabled: true
   }
 };
