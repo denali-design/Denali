d6d1aa0ea87ce47524b823c133834d724d79e009
Pete Medina <petemedinacreative@gmail.com>
2024-02-01 13:48:07 -0800

    feat(component): add TagGroup
    - Add TagGroup component and storybook docs

--- src/components/TagGroup/TagGroup.tsx
+++ src/components/TagGroup/TagGroup.tsx
@@ -5,8 +5,67 @@
  * @since 1.0.0
  */
 
-const TagGroup: React.FC = () => {
-  return 'TagGroup Coming Soon';
-};
+import {
+  TagGroup as AriaTagGroup,
+  TagList,
+  Tag as AriaTag,
+  Label
+} from 'react-aria-components';
+import type {
+  TagGroupProps,
+  TagListProps,
+  TagProps
+} from 'react-aria-components';
+import { Button, Text } from 'react-aria-components';
+import '../../assets/css/App.css';
+import './TagGroup.css';
+
+interface AriaTagGroupProps<T>
+  extends Omit<TagGroupProps, 'children'>,
+    Pick<TagListProps<T>, 'items' | 'children' | 'renderEmptyState'> {
+  label?: string;
+  description?: string;
+  errorMessage?: string;
+}
+
+function TagGroup<T extends object>({
+  label,
+  description,
+  errorMessage,
+  items,
+  children,
+  renderEmptyState,
+  ...props
+}: AriaTagGroupProps<T>) {
+  return (
+    <AriaTagGroup className="tag-group" {...props}>
+      <Label className="tag-group__label">{label}</Label>
+      <TagList
+        className="tag-group__list"
+        items={items}
+        renderEmptyState={renderEmptyState}
+      >
+        {children}
+      </TagList>
+      {description && <Text slot="description">{description}</Text>}
+      {errorMessage && <Text slot="errorMessage">{errorMessage}</Text>}
+    </AriaTagGroup>
+  );
+}
+
+function Tag({ children, ...props }: TagProps) {
+  let textValue = typeof children === 'string' ? children : undefined;
+  return (
+    <AriaTag className="tag-group__tag" textValue={textValue} {...props}>
+      {({ allowsRemoving }) => (
+        <>
+          {children}
+          {allowsRemoving && <Button slot="remove">ⓧ</Button>}
+        </>
+      )}
+    </AriaTag>
+  );
+}
+
 export default TagGroup;
-export { TagGroup };
+export { Tag };
