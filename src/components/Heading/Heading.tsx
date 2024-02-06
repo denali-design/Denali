11efc47a8d82378403226c9c8367ea73157163b3
Trevor Greenleaf <trevor.greenleaf@yahooinc.com>
2024-02-05 18:07:51 -0800

    refactor(heading size options)

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -12,12 +12,12 @@ import '../../assets/css/App.css';
  * @extends {HTMLAttributes<HTMLElement>}
  *
  * @property {1|2|3|4|5|6} [level] - The semantic level of the heading.
- * @property {1|2|3|4|5|6} [size] - The visual style level of the heading.
+ * @property {h1|h2|h3|h4|h5|h6|title|body|label} [size] - The visual style level of the heading.
  * @property {'article'|'app'|'display'} [type] - The type of heading, which defines the style.
  */
 export interface HeadingProps extends HTMLAttributes<HTMLElement> {
   level?: 1 | 2 | 3 | 4 | 5 | 6;
-  size?: 1 | 2 | 3 | 4 | 5 | 6;
+  size?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'title' | 'body' | 'label';
   type?: 'article' | 'app' | 'display';
 }
 
@@ -48,11 +48,13 @@ export const Heading = memo(
     const {
       children,
       level = 1,
-      size = level,
+      size,
       type = 'app',
       className,
       ...domProps
     } = props;
+
+    const effectiveSize = size || HeadingTagMap[level];
 
     // Configuration object for Tailwind Variants, defined internally.
     const headingStyles = tv({
@@ -60,28 +62,37 @@ export const Heading = memo(
       variants: {
         type: {
           article: {
-            1: 'text-6xl my-4 font-bold',
-            2: 'text-3xl mt-6 mb-3 font-medium',
-            3: 'text-2xl my-2 font-medium',
-            4: 'text-xl my-2 font-medium',
-            5: 'text-lg my-1 font-medium',
-            6: 'text-base my-1 font-medium'
+            h1: 'text-6xl my-4 font-bold',
+            h2: 'text-3xl mt-6 mb-3 font-medium',
+            h3: 'text-2xl my-2 font-medium',
+            h4: 'text-xl my-2 font-medium',
+            h5: 'text-lg my-1 font-medium',
+            h6: 'text-base my-1 font-medium',
+            body: 'text-sm font-normal',
+            title: 'text-sm font-bold',
+            label: 'text-xs font-normal'
           },
           app: {
-            1: 'text-4xl font-bold',
-            2: 'text-3xl font-medium',
-            3: 'text-2xl font-medium',
-            4: 'text-xl font-medium',
-            5: 'text-lg font-medium',
-            6: 'text-base font-medium'
+            h1: 'text-4xl font-bold',
+            h2: 'text-2xl font-bold',
+            h3: 'text-xl font-medium',
+            h4: 'text-lg font-medium',
+            h5: 'text-base font-medium',
+            h6: 'text-base font-normal',
+            body: 'text-sm font-normal',
+            title: 'text-sm font-bold',
+            label: 'text-xs font-normal'
           },
           display: {
-            1: 'text-9xl font-bold',
-            2: 'text-8xl font-bold',
-            3: 'text-7xl font-bold',
-            4: 'text-6xl font-bold',
-            5: 'text-5xl font-bold',
-            6: 'text-4xl font-bold'
+            h1: 'text-9xl font-bold',
+            h2: 'text-8xl font-bold',
+            h3: 'text-7xl font-bold',
+            h4: 'text-6xl font-bold',
+            h5: 'text-5xl font-bold',
+            h6: 'text-4xl font-bold',
+            body: 'text-sm font-normal',
+            title: 'text-sm font-bold',
+            label: 'text-xs font-normal'
           }
         }
       }
@@ -96,7 +107,7 @@ export const Heading = memo(
      * Computes the Tailwind CSS class string based on the given props using the headingStyles object.
      */
     const computedClass =
-      headingStyles.variants.type[type]?.[size] || headingStyles.base;
+      headingStyles.variants.type[type]?.[effectiveSize] || headingStyles.base;
 
     return (
       <Element
